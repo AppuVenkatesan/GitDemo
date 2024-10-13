a4deeb0e017e0f53044a5cb8cb1f8530f6bb71e3
appu <appu.v222@gmail.com>
2024-10-13 19:00:20 +0530

    first commit

---
+++ src/addition.java
@@ -0,0 +1,27 @@
+
+import packagedemo.*;
+
+public class addition extends accessmodifier {
+
+	public static void main(String[] args) {
+		
+		int a = add(5,6);
+		
+		System.out.println(a);
+		
+		
+		
+	}
+		
+		
+		
+		public static int add(int x,int y) {
+			
+		int z=x+y;
+		
+		return z;
+	
+	}
+	
+	
+}
